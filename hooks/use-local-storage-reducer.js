bd6c6f52655c946b888afd863b009bb09dbf6943
barto <xhaltx4@gmail.com>
2023-03-17 00:37:27 +0100

    working on stripe-1

--- hooks/use-local-storage-reducer.js
+++ hooks/use-local-storage-reducer.js
@@ -1,5 +1,5 @@
 import { useEffect, useReducer, useRef } from 'react';
-import { isClient } from '@/lib/utils';
+import { isClient } from '@/utils';
 
 const useLocalStorageReducer = (key = '', reducer, initialValue = null) => {
   const [state, dispatch] = useReducer(reducer, initialValue, () => {
